3e528695fbc894140cd5bb2f13ec77773a921626
Pedro Alexis Gonzalez Espinoza <pgonzaleze@vortex.corp>
2025-04-01 16:46:07 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>eSIm megacable</h1>
+      <div className="card">
+       
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A molestiae assumenda in repellat? Odio, in? Blanditiis officia omnis ducimus alias sapiente, consequatur magnam sed corrupti nobis quo possimus quod eligendi.
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
